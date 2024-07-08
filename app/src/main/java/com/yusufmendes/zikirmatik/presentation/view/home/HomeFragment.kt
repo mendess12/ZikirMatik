e7f0480e0018065397089f1e55c3a904842ae770
Yusuf Mendeş <yusufmendes123@gmail.com>
2024-07-08 14:37:35 +0300

    Add bottom sheet dialog

--- app/src/main/java/com/yusufmendes/zikirmatik/presentation/view/home/HomeFragment.kt
+++ app/src/main/java/com/yusufmendes/zikirmatik/presentation/view/home/HomeFragment.kt
@@ -3,11 +3,42 @@ package com.yusufmendes.zikirmatik.presentation.view.home
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.snackbar.Snackbar
 import com.yusufmendes.zikirmatik.R
+import com.yusufmendes.zikirmatik.databinding.BottomSheetDialogBinding
+import com.yusufmendes.zikirmatik.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var bottomSheetDialogBinding: BottomSheetDialogBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHomeBinding.bind(view)
+
+        binding.buttonSave.setOnClickListener {
+            bottomSheetDialog()
+        }
+    }
+
+    private fun bottomSheetDialog() {
+        val dialog = BottomSheetDialog(requireContext())
+        bottomSheetDialogBinding = BottomSheetDialogBinding.inflate(layoutInflater)
+        dialog.setContentView(bottomSheetDialogBinding.root)
+        dialog.show()
+        bottomSheetDialogBinding.btdSave.setOnClickListener() {
+            val counterName = bottomSheetDialogBinding.etCounterName.text.toString()
+            if (counterName.isEmpty()) {
+                Snackbar.make(requireView(), "Lütfen bir isim giriniz", Snackbar.LENGTH_SHORT)
+                    .show()
+            } else {
+                Snackbar.make(requireView(), "$counterName kaydedildi", Snackbar.LENGTH_SHORT)
+                    .show()
+                dialog.dismiss()
+            }
+        }
     }
 }
+

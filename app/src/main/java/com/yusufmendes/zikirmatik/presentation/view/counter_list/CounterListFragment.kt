495f9fcb005d4066586702de63b77c58aae64e0e
Yusuf Mendeş <yusufmendes123@gmail.com>
2024-07-08 13:01:49 +0300

    Add counter list design

---
+++ app/src/main/java/com/yusufmendes/zikirmatik/presentation/view/counter_list/CounterListFragment.kt
@@ -0,0 +1,13 @@
+package com.yusufmendes.zikirmatik.presentation.view.counter_list
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.yusufmendes.zikirmatik.R
+
+class CounterListFragment : Fragment(R.layout.fragment_counter_list) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}

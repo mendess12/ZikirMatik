111437873944f6b32fd1ce6cbc63da706a6bfdcb
Yusuf Mendeş <yusufmendes123@gmail.com>
2024-07-08 22:50:12 +0300

    Add hadith adapter

---
+++ app/src/main/java/com/yusufmendes/zikirmatik/presentation/adapter/HadithAdapter.kt
@@ -0,0 +1,41 @@
+package com.yusufmendes.zikirmatik.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.yusufmendes.zikirmatik.data.model.Hadith
+import com.yusufmendes.zikirmatik.databinding.ItemHadithBinding
+
+class HadithAdapter : RecyclerView.Adapter<HadithAdapter.HadithViewHolder>() {
+
+    private var hadithList = ArrayList<Hadith>()
+
+    inner class HadithViewHolder(private val binding: ItemHadithBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(hadith: Hadith) {
+            with(binding) {
+                tvItemHadithTitle.text = hadith.title
+                tvItemHadithContent.text = hadith.content
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HadithViewHolder {
+        val binding = ItemHadithBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return HadithViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = hadithList.size
+
+    override fun onBindViewHolder(holder: HadithViewHolder, position: Int) {
+        val hadith = hadithList[position]
+        holder.bind(hadith)
+    }
+
+    fun updateHadithList(updateHadithList: List<Hadith>) {
+        hadithList.clear()
+        hadithList.addAll(updateHadithList)
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/com/yusufmendes/zikirmatik/presentation/view/hadith/HadithFragment.kt
+++ app/src/main/java/com/yusufmendes/zikirmatik/presentation/view/hadith/HadithFragment.kt
@@ -3,16 +3,44 @@ package com.yusufmendes.zikirmatik.presentation.view.hadith
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.yusufmendes.zikirmatik.R
 import com.yusufmendes.zikirmatik.databinding.FragmentHadithBinding
+import com.yusufmendes.zikirmatik.presentation.adapter.HadithAdapter
+import com.yusufmendes.zikirmatik.util.extensions.showSnackbar
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class HadithFragment : Fragment(R.layout.fragment_hadith) {
 
     private lateinit var binding: FragmentHadithBinding
+    private lateinit var hadithAdapter: HadithAdapter
+    private val viewModel: HadithFragmentViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentHadithBinding.bind(view)
+
+        hadithAdapter = HadithAdapter()
+        with(binding) {
+            rvHadith.setHasFixedSize(true)
+            rvHadith.layoutManager = LinearLayoutManager(requireContext())
+            rvHadith.adapter = hadithAdapter
+        }
+
+        viewModel.getHadithList()
+        observeData()
+    }
+
+
+    private fun observeData() {
+        viewModel.hadithLiveData.observe(viewLifecycleOwner) {
+            if (it != null) {
+                hadithAdapter.updateHadithList(it)
+            } else {
+                view?.showSnackbar("Hadis ve Ayet listesi boş!")
+            }
+        }
     }
 }

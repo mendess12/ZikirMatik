6ebc2342d7e1533817bc7c503da8989e6baec775
Yusuf Mendeş <yusufmendes123@gmail.com>
2024-07-08 22:45:42 +0300

    Add data class for hadith and name of allah

---
+++ app/src/main/java/com/yusufmendes/zikirmatik/data/model/Hadith.kt
@@ -0,0 +1,6 @@
+package com.yusufmendes.zikirmatik.data.model
+
+data class Hadith(
+    val title: String? = "",
+    val content: String? = ""
+)
---
+++ app/src/main/java/com/yusufmendes/zikirmatik/data/model/Name.kt
@@ -0,0 +1,7 @@
+package com.yusufmendes.zikirmatik.data.model
+
+data class Name(
+    val turkishName: String? = "",
+    val arabicName: String? = "",
+    val mean: String? = ""
+)

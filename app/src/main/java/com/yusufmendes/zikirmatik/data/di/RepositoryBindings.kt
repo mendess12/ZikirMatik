5a357f1d06d10c2d5a6bed286169c930e5ffaed0
Yusuf Mendeş <yusufmendes123@gmail.com>
2024-07-09 12:38:56 +0300

    Add name of allah repository binding fun

--- app/src/main/java/com/yusufmendes/zikirmatik/data/di/RepositoryBindings.kt
+++ app/src/main/java/com/yusufmendes/zikirmatik/data/di/RepositoryBindings.kt
@@ -1,6 +1,8 @@
 package com.yusufmendes.zikirmatik.data.di
 
 import com.yusufmendes.zikirmatik.data.repository.HadithRepositoryImpl
+import com.yusufmendes.zikirmatik.domain.repos.NameOfAllahRepository
+import com.yusufmendes.zikirmatik.data.repository.NameOfAllahRepositoryImpl
 import com.yusufmendes.zikirmatik.domain.repos.HadithRepository
 import dagger.Binds
 import dagger.Module
@@ -15,4 +17,8 @@ abstract class RepositoryBindings {
     @Singleton
     @Binds
     abstract fun bindHadithRepository(hadithRepository: HadithRepositoryImpl): HadithRepository
+
+    @Singleton
+    @Binds
+    abstract fun bindNameOfAllahRepository(nameOfAllahRepository: NameOfAllahRepositoryImpl): NameOfAllahRepository
 }

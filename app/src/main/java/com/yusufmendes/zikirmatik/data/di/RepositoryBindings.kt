d34a811e1680c85e52fad742e08dd4726ea61914
Yusuf Mendeş <yusufmendes123@gmail.com>
2024-07-10 22:37:25 +0300

    Add repository binding for counter list and home

--- app/src/main/java/com/yusufmendes/zikirmatik/data/di/RepositoryBindings.kt
+++ app/src/main/java/com/yusufmendes/zikirmatik/data/di/RepositoryBindings.kt
@@ -1,9 +1,13 @@
 package com.yusufmendes.zikirmatik.data.di
 
+import com.yusufmendes.zikirmatik.data.repository.CounterRepositoryImpl
 import com.yusufmendes.zikirmatik.data.repository.HadithRepositoryImpl
+import com.yusufmendes.zikirmatik.data.repository.HomeRepositoryImpl
 import com.yusufmendes.zikirmatik.domain.repos.NameOfAllahRepository
 import com.yusufmendes.zikirmatik.data.repository.NameOfAllahRepositoryImpl
+import com.yusufmendes.zikirmatik.domain.repos.CounterRepository
 import com.yusufmendes.zikirmatik.domain.repos.HadithRepository
+import com.yusufmendes.zikirmatik.domain.repos.HomeRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -21,4 +25,13 @@ abstract class RepositoryBindings {
     @Singleton
     @Binds
     abstract fun bindNameOfAllahRepository(nameOfAllahRepository: NameOfAllahRepositoryImpl): NameOfAllahRepository
+
+    @Singleton
+    @Binds
+    abstract fun bindHomeRepository(homeRepository: HomeRepositoryImpl): HomeRepository
+
+    @Singleton
+    @Binds
+    abstract fun bindCounterRepository(counterRepository: CounterRepositoryImpl): CounterRepository
+
 }

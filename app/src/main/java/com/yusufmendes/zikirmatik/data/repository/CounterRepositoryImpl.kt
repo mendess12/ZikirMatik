de3bd10ddf7218c0098f17725e18b7a145276709
Yusuf Mendeş <yusufmendes123@gmail.com>
2024-07-10 22:38:05 +0300

    Add counter repository

---
+++ app/src/main/java/com/yusufmendes/zikirmatik/data/repository/CounterRepositoryImpl.kt
@@ -0,0 +1,12 @@
+package com.yusufmendes.zikirmatik.data.repository
+
+import com.yusufmendes.zikirmatik.data.model.CounterEntity
+import com.yusufmendes.zikirmatik.data.source.local.CounterDao
+import com.yusufmendes.zikirmatik.domain.repos.CounterRepository
+import javax.inject.Inject
+
+class CounterRepositoryImpl @Inject constructor(
+    private val counterDao: CounterDao
+) : CounterRepository {
+    override suspend fun getCounterList(): List<CounterEntity> = counterDao.getAllCounters()
+}
---
+++ app/src/main/java/com/yusufmendes/zikirmatik/domain/repos/CounterRepository.kt
@@ -0,0 +1,8 @@
+package com.yusufmendes.zikirmatik.domain.repos
+
+import com.yusufmendes.zikirmatik.data.model.CounterEntity
+
+interface CounterRepository {
+
+    suspend fun getCounterList(): List<CounterEntity>
+}

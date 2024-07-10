b0552f128eac50c97c394785b07d5890f4e983f5
Yusuf Mendeş <yusufmendes123@gmail.com>
2024-07-10 22:40:39 +0300

    Add home fragment

--- app/src/main/java/com/yusufmendes/zikirmatik/presentation/view/home/HomeFragment.kt
+++ app/src/main/java/com/yusufmendes/zikirmatik/presentation/view/home/HomeFragment.kt
@@ -1,21 +1,29 @@
 package com.yusufmendes.zikirmatik.presentation.view.home
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.provider.Settings
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomsheet.BottomSheetDialog
-import com.google.android.material.snackbar.Snackbar
 import com.yusufmendes.zikirmatik.R
+import com.yusufmendes.zikirmatik.data.model.CounterEntity
 import com.yusufmendes.zikirmatik.databinding.BottomSheetDialogBinding
 import com.yusufmendes.zikirmatik.databinding.FragmentHomeBinding
 import com.yusufmendes.zikirmatik.util.extensions.showSnackbar
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 @AndroidEntryPoint
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private lateinit var binding: FragmentHomeBinding
     private lateinit var bottomSheetDialogBinding: BottomSheetDialogBinding
+    private val viewModel: HomeFragmentViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -24,8 +32,22 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         binding.buttonSave.setOnClickListener {
             bottomSheetDialog()
         }
+
+        binding.buttonGoList.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_counterListFragment)
+        }
+        observeLiveData()
     }
 
+    private fun observeLiveData() {
+        viewModel.addCounterLiveData.observe(viewLifecycleOwner) {
+            if (it != null) {
+                view?.showSnackbar("Zikiriniz Kaydedildi")
+            }
+        }
+    }
+
+    @SuppressLint("HardwareIds")
     private fun bottomSheetDialog() {
         val dialog = BottomSheetDialog(requireContext())
         bottomSheetDialogBinding = BottomSheetDialogBinding.inflate(layoutInflater)
@@ -36,7 +58,23 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
             if (counterName.isEmpty()) {
                 view?.showSnackbar("Lütfen bir isim giriniz")
             } else {
-                view?.showSnackbar("$counterName kaydedildi")
+                val calendar = Calendar.getInstance()
+                val timeFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+                val currentTime = timeFormat.format(calendar.time)
+                val date = currentTime
+
+                val userId = Settings.Secure.getString(
+                    requireContext().contentResolver,
+                    Settings.Secure.ANDROID_ID
+                )
+
+                val counter = binding.txCounterInfo.text.toString().toInt()
+                val id = 3
+                val title = counterName
+
+                val counterEntity = CounterEntity(userId, id, title, counter, date)
+                viewModel.addCounter(counterEntity)
+                dialog.dismiss()
             }
         }
     }

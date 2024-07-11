39b8e9528726c3189374a6d73e6d508c5cb2fa9e
Yusuf Mendeş <yusufmendes123@gmail.com>
2024-07-11 22:25:04 +0300

    Add shared preferences for count

---
+++ app/src/main/java/com/yusufmendes/zikirmatik/util/storage/SharedPrefManager.kt
@@ -0,0 +1,21 @@
+package com.yusufmendes.zikirmatik.util.storage
+
+import android.content.Context
+import javax.inject.Inject
+
+class SharedPrefManager @Inject constructor(
+    private val context: Context
+) {
+
+    val sharedPreferences = context.getSharedPreferences("CounterPref", Context.MODE_PRIVATE)
+    val editor = sharedPreferences.edit()
+
+    fun getCounter(): Int {
+        return sharedPreferences.getInt("counter", 0)
+    }
+
+    fun saveCounter(counter: Int) {
+        editor.putInt("counter", counter)
+        editor.apply()
+    }
+}

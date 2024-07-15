6c30444e4cabd96eaa171ef8cff156815755ede5
Yusuf Mendeş <yusufmendes123@gmail.com>
2024-07-15 13:05:16 +0300

    Add delete counter observe live data and show alert dialog

--- app/src/main/java/com/yusufmendes/zikirmatik/presentation/view/counter_list/CounterListFragment.kt
+++ app/src/main/java/com/yusufmendes/zikirmatik/presentation/view/counter_list/CounterListFragment.kt
@@ -2,10 +2,13 @@ package com.yusufmendes.zikirmatik.presentation.view.counter_list
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.yusufmendes.zikirmatik.R
+import com.yusufmendes.zikirmatik.data.model.CounterEntity
 import com.yusufmendes.zikirmatik.databinding.FragmentCounterListBinding
 import com.yusufmendes.zikirmatik.presentation.adapter.CounterAdapter
 import com.yusufmendes.zikirmatik.util.extensions.showSnackbar
@@ -22,7 +25,7 @@ class CounterListFragment : Fragment(R.layout.fragment_counter_list) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentCounterListBinding.bind(view)
 
-        counterAdapter = CounterAdapter()
+        counterAdapter = CounterAdapter(::deleteCounter)
         with(binding) {
             rvCounter.setHasFixedSize(true)
             rvCounter.layoutManager = LinearLayoutManager(requireContext())
@@ -34,6 +37,10 @@ class CounterListFragment : Fragment(R.layout.fragment_counter_list) {
 
     }
 
+    private fun deleteCounter(counterEntity: CounterEntity) {
+        showAlertDialog(counterEntity)
+    }
+
     private fun observeLiveData() {
         viewModel.counterListLiveData.observe(viewLifecycleOwner) {
             if (it != null) {
@@ -42,5 +49,30 @@ class CounterListFragment : Fragment(R.layout.fragment_counter_list) {
                 view?.showSnackbar("Zikir listeniz boş!")
             }
         }
+        viewModel.deleteCountLiveData.observe(viewLifecycleOwner) {
+            if (it != null) {
+                view?.showSnackbar("Zikir silindi")
+            } else {
+                view?.showSnackbar("Zikir listeniz boş!")
+            }
+        }
+    }
+
+    private fun showAlertDialog(counterEntity: CounterEntity) {
+
+        val dialogView = layoutInflater.inflate(R.layout.alert_dialog, null)
+        val alertDialog = AlertDialog.Builder(requireContext())
+            .setView(dialogView)
+        val dialog = alertDialog.create()
+        dialog.setCancelable(false)
+
+        dialogView.findViewById<Button>(R.id.ad_btn_cancel).setOnClickListener {
+            dialog.dismiss()
+        }
+        dialogView.findViewById<Button>(R.id.ad_btn_delete).setOnClickListener {
+            viewModel.deleteCounter(counterEntity)
+            dialog.dismiss()
+        }
+        dialog.show()
     }
 }
--- app/src/main/java/com/yusufmendes/zikirmatik/presentation/view/counter_list/CounterListFragmentViewModel.kt
+++ app/src/main/java/com/yusufmendes/zikirmatik/presentation/view/counter_list/CounterListFragmentViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.yusufmendes.zikirmatik.data.model.CounterEntity
+import com.yusufmendes.zikirmatik.domain.usecases.counterlist.DeleteCountUseCase
 import com.yusufmendes.zikirmatik.domain.usecases.counterlist.GetCounterListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -11,13 +12,21 @@ import javax.inject.Inject
 
 @HiltViewModel
 class CounterListFragmentViewModel @Inject constructor(
-    private val counterListUseCase: GetCounterListUseCase
+    private val counterListUseCase: GetCounterListUseCase,
+    private val deleteCountUseCase: DeleteCountUseCase
 ) : ViewModel() {
 
     var counterListLiveData = MutableLiveData<List<CounterEntity>>()
+    var deleteCountLiveData = MutableLiveData<Unit>()
 
     fun getCounterList() = viewModelScope.launch {
         val result = counterListUseCase.getCounterList()
         counterListLiveData.postValue(result)
+    }
+
+    fun deleteCounter(counterEntity: CounterEntity) = viewModelScope.launch {
+        val result = deleteCountUseCase.deleteCounter(counterEntity)
+        deleteCountLiveData.postValue(result)
+        getCounterList()
     }
 }

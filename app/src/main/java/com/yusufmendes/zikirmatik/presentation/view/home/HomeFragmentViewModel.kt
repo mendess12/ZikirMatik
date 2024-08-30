315b4bd65b313e5019bd1d0a60472a7d48332b44
yusufmendes <yusufmendes123@gmail.com>
2024-08-30 22:26:57 +0300

    Add view model for play store functions

--- app/src/main/java/com/yusufmendes/zikirmatik/presentation/view/home/HomeFragmentViewModel.kt
+++ app/src/main/java/com/yusufmendes/zikirmatik/presentation/view/home/HomeFragmentViewModel.kt
@@ -1,23 +1,27 @@
 package com.yusufmendes.zikirmatik.presentation.view.home
 
+import android.content.Context
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.yusufmendes.zikirmatik.data.model.CounterEntity
 import com.yusufmendes.zikirmatik.domain.usecases.home.AddCounterUseCase
+import com.yusufmendes.zikirmatik.domain.usecases.home.OpenPlayStoreUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeFragmentViewModel @Inject constructor(
-    private val addCounterUseCase: AddCounterUseCase
+    private val addCounterUseCase: AddCounterUseCase,
+    private val playStoreUseCase: OpenPlayStoreUseCase
 ) : ViewModel() {
 
     var addCounterLiveData = MutableLiveData<Unit>()
     var count = 0
     val countLiveData = MutableLiveData<Int>()
     val deleteCounterLiveData = MutableLiveData<Int>()
+    var playStoreLiveData = MutableLiveData<Unit>()
 
     fun getCounter() {
         count++
@@ -32,5 +36,10 @@ class HomeFragmentViewModel @Inject constructor(
     fun addCounter(counterEntity: CounterEntity) = viewModelScope.launch {
         val result = addCounterUseCase.addCounter(counterEntity)
         addCounterLiveData.postValue(result)
+    }
+
+    fun openPlayStore(context: Context, developerId: String) = viewModelScope.launch {
+        val result = playStoreUseCase.openPlayStore(context, developerId)
+        playStoreLiveData.postValue(result)
     }
 }

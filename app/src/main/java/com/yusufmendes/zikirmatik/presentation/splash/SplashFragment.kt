59b375a9ec1eba5abfdfbcb5abd264a036bb2a59
yusufmendes <yusufmendes123@gmail.com>
2024-09-16 03:03:21 +0300

    Add splash screen

---
+++ app/src/main/java/com/yusufmendes/zikirmatik/presentation/splash/SplashFragment.kt
@@ -0,0 +1,23 @@
+package com.yusufmendes.zikirmatik.presentation.splash
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import com.yusufmendes.zikirmatik.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashFragment : Fragment(R.layout.fragment_splash) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(2000)
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+        }
+    }
+}

faea304dc7818b28c107f9a92d8e3a3d79c6d843
yusufmendes <yusufmendes123@gmail.com>
2024-09-19 17:27:53 +0300

    Update minSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "com.yusufmendes.zikirmatik"
-        minSdk = 24
+        minSdk = 31
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"

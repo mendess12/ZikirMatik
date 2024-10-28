344856e2a428e53badb435388d56567ca406e669
yusufmendes <yusufmendes123@gmail.com>
2024-10-28 13:20:40 +0300

    Add parcelable for namaz model

--- app/src/main/java/com/yusufmendes/zikirmatik/data/model/Namaz.kt
+++ app/src/main/java/com/yusufmendes/zikirmatik/data/model/Namaz.kt
@@ -1,8 +1,12 @@
 package com.yusufmendes.zikirmatik.data.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Namaz(
     val id: String? = "",
     val title: String? = "",
     val description: String? = "",
     val image_url: String = ""
-)
+):Parcelable

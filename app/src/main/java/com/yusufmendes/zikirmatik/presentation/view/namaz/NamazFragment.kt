2ff34de787f0c81867adc1a50b809b1717f74934
yusufmendes <yusufmendes123@gmail.com>
2024-10-28 11:41:01 +0300

    Add namaz observe livedata

--- app/src/main/java/com/yusufmendes/zikirmatik/presentation/view/namaz/NamazFragment.kt
+++ app/src/main/java/com/yusufmendes/zikirmatik/presentation/view/namaz/NamazFragment.kt
@@ -3,15 +3,54 @@ package com.yusufmendes.zikirmatik.presentation.view.namaz
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.fragment.app.viewModels
 import com.yusufmendes.zikirmatik.R
 import com.yusufmendes.zikirmatik.databinding.FragmentNamazBinding
+import com.yusufmendes.zikirmatik.presentation.adapter.NamazAdapter
+import com.yusufmendes.zikirmatik.util.extensions.gone
+import com.yusufmendes.zikirmatik.util.extensions.showSnackbar
+import com.yusufmendes.zikirmatik.util.extensions.visible
+import com.yusufmendes.zikirmatik.util.resources.Resource
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class NamazFragment : Fragment(R.layout.fragment_namaz) {
 
     private lateinit var binding: FragmentNamazBinding
+    private val viewModel: NamazFragmentViewModel by viewModels()
+    private lateinit var namazAdapter: NamazAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentNamazBinding.bind(view)
+
+        namazAdapter = NamazAdapter()
+        with(binding) {
+            rvNamaz.setHasFixedSize(true)
+            rvNamaz.adapter = namazAdapter
+        }
+
+        viewModel.getNamazList()
+        observeLiveData()
+    }
+
+    private fun observeLiveData() {
+        viewModel.namazLiveData.observe(viewLifecycleOwner) {
+            when (it) {
+                Resource.Loading -> {
+                    binding.pbNamaz.visible()
+                }
+
+                is Resource.Success -> {
+                    namazAdapter.updateNamazList(it.data)
+                    binding.pbNamaz.gone()
+                }
+
+                is Resource.Error -> {
+                    view?.showSnackbar(it.errorMessage.toString())
+                    binding.pbNamaz.gone()
+                }
+            }
+        }
     }
 }
